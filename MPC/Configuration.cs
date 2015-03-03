9f5846334f0d49a902c01aed169a4ccec6441ac2
projectgoav <edm22@hotmail.co.uk>
2015-03-03 21:23:46 +0000

    Cleaned internal MPC structure

---
+++ MPC/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPC
+{
+    public class Configuration
+    {
+
+        public const string CONFIG_FILE = "Config.ini";
+
+        public string HeaderTemplate { get; set; }
+        public string FooterTemplate { get; set; }
+        public string NavTemplate { get; set; }
+        public string ModalTemplate { get; set; }
+
+        public string[] Pages { get; set; }
+
+        public string SourceLocation { get; set; }
+        public string PublishLocation { get; set; }
+    }
+}
